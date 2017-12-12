015525978d57e79ca187698bc4906313cd68daf7
Momper14 <moritz.momper@gmail.com>
2017-12-12 23:26:46 +0100

    Update

--- src/DBErrorHandler.java
+++ src/DBErrorHandler.java
@@ -3,28 +3,28 @@ import org.xml.sax.ErrorHandler;
 import org.xml.sax.SAXException;
 import org.xml.sax.SAXParseException;
 
-    public class DBErrorHandler implements ErrorHandler {
+public class DBErrorHandler implements ErrorHandler {
 
-        @Override
-        public void warning(SAXParseException ep) throws SAXException {
-            System.out.println("Parser meldet WARNUNG: " + ep.toString());
-            System.out.println("an der Entity        : " + ep.getPublicId());
-            System.out.println("Zeile,Spalte         : " + ep.getLineNumber() + "," + ep.getColumnNumber());
-        }
-
-        @Override
-        public void error(SAXParseException ep) throws SAXException {
-            System.out.println("Parser meldet FEHLER : " + ep.toString());
-            System.out.println("an der Entity        : " + ep.getPublicId());
-            System.out.println("Zeile,Spalte         : " + ep.getLineNumber() + "," + ep.getColumnNumber());
-            throw ep;
-        }
-
-        @Override
-        public void fatalError(SAXParseException ep) throws SAXException {
-            System.out.println("Fataler FEHLER !!!   : " + ep.toString());
-            System.out.println("an der Entity        : " + ep.getPublicId());
-            System.out.println("Zeile,Spalte         : " + ep.getLineNumber() + "," + ep.getColumnNumber());
-            throw ep;
-        }
+    @Override
+    public void warning(SAXParseException ep) throws SAXException {
+        System.out.println("Parser meldet WARNUNG: " + ep.toString());
+        System.out.println("an der Entity        : " + ep.getPublicId());
+        System.out.println("Zeile,Spalte         : " + ep.getLineNumber() + "," + ep.getColumnNumber());
     }
+
+    @Override
+    public void error(SAXParseException ep) throws SAXException {
+        System.out.println("Parser meldet FEHLER : " + ep.toString());
+        System.out.println("an der Entity        : " + ep.getPublicId());
+        System.out.println("Zeile,Spalte         : " + ep.getLineNumber() + "," + ep.getColumnNumber());
+        throw ep;
+    }
+
+    @Override
+    public void fatalError(SAXParseException ep) throws SAXException {
+        System.out.println("Fataler FEHLER !!!   : " + ep.toString());
+        System.out.println("an der Entity        : " + ep.getPublicId());
+        System.out.println("Zeile,Spalte         : " + ep.getLineNumber() + "," + ep.getColumnNumber());
+        throw ep;
+    }
+}
